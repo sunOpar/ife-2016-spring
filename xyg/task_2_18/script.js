b84e21840f944372b41497fd7666b744c488600b
jason <xygjsj@139.com>
2016-04-22 19:11:41 +0800

    xyg提交19

--- xyg/task_2_18/script.js
+++ xyg/task_2_18/script.js
@@ -1,24 +1,75 @@
-function $(id){
+(function() {
+	var EventUtil = {
+		addHandler: function(element, type, handler) {
+			if (element.addEventListener) {
+				element.addEventListener(type, handler, false);
+			} else if (element.attachHandler) {
+				element.attachHandler('on' + type, handler);
+			} else {
+				element['on' + type] = handler;
+			}
+		},
+		removeHandler: function(element, type, handler) {
+			if (element.removeEventLinstener) {
+				element.removeEventLinstener(type, handler, false);
+			} else if (element.detachHandler) {
+				element.detachHandler('on' + type, handler);
+			} else {
+				element['on' + type] = null;
+			}
+		}
+	};
+	var btn = document.querySelectorAll('button');
+	EventUtil.addHandler(btn[0], 'click', leftIn);
+	EventUtil.addHandler(btn[1], 'click', rightIn);
+	EventUtil.addHandler(btn[2], 'click', leftOut);
+	EventUtil.addHandler(btn[3], 'click', rightOut);
+})();
+
+
+function $(id) {
 	return document.getElementById(id);
 }
+
+function judeg(argument) {
+	var prompt = document.getElementById('prompt');
+	var flag = true;
+	var patt = /^[0-9]+$/;
+	if (!patt.test(argument)) {
+		prompt.textContent = '请输入【一个】数字';
+		flag = false;
+	}
+	return flag;
+}
+
 function leftIn() {
 	var num = $('text').value;
-	var fir = document.createElement('li');
-	var las = document.createTextNode(num);
-	fir.appendChild(las);
-	$('show').insertBefore(fir,$('show').firstChild);
+	var flag = judeg(num);
+	if (flag) {
+		var fir = document.createElement('li');
+		var las = document.createTextNode(num);
+		fir.appendChild(las);
+		$('show').insertBefore(fir, $('show').firstChild);
+	}
 }
-function rightIn(){
- 	var num = $('text').value;
- 	var fir = document.createElement('li');
- 	var las = document.createTextNode(num);
- 	fir.appendChild(las);
- 	$('show').appendChild(fir);
- }
- function leftOut(){
- 	alert($('show').firstChild.textContent);
- 	$('show').removeChild($('show').firstChild);
- }
- function rightOut(){
- 	$('show').removeChild($('show').lastElementChild);
- }
+
+function rightIn() {
+	var num = $('text').value;
+	var flag = judeg(num);
+	if (flag) {
+		var fir = document.createElement('li');
+		var las = document.createTextNode(num);
+		fir.appendChild(las);
+		$('show').appendChild(fir);
+	}
+}
+
+function leftOut() {
+	alert($('show').firstChild.textContent);
+	$('show').removeChild($('show').firstElementChild);
+}
+
+function rightOut() {
+	alert($('show').lastElementChild.textContent);
+	$('show').removeChild($('show').lastElementChild);
+}
