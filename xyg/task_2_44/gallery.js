8c32138ab37a8c756ce549c0762f3e2f72c8b900
xygjsj@139.com <xygjsj@139.com>
2016-05-18 18:01:56 +0800

    bjb瀑布流

--- xyg/task_2_44/gallery.js
+++ xyg/task_2_44/gallery.js
@@ -1,55 +1,83 @@
 /**
  * 瀑布布局
- * @param {Array} imgsHtml 图片html格式的数组
- * @param {node} wrap     布局的包裹层
- * @param {Number} gap      图片的margin值
- * @param {number} col      图片的列数
+ * @param {Array}  domImgs       图片DOM的数组
+ * @param {node}   wrap           布局的包裹层
+ * @param {Number} gap            图片的margin值
+ * @param {number} columnNumber   图片的列数
+ * @param {number} wrapWidth      包裹层的宽度
  */
-function Gallery(imgsHtml, wrap, gap, col) {
-	this.imgsHtml = imgsHtml;
+function Gallery(domImgs, wrap, gap, columnNumber) {
+	this.domImgs = domImgs;
 	this.wrap = wrap;
 	this.gap = gap;
-	this.col = col;
+	this.columnNumber= columnNumber;
+	this.wrapWidth = this.wrap.clientWidth;	
+	this.childColumnsLength = [];
+
 }
 /**
  * 负责渲染图片的css样式
- * @param  {[type]} imgs     [description]
  * @param  {[type]} imgWidth [description]
  * @return {[type]}          [description]
  */
-Gallery.prototype.renderStyle = function(imgs, imgWidth, childs) {
-	var wrapWidth = this.wrap.clientWidth;
-	var j = imgs.length;
-	for (var i = 0; i < j; i++) {
-		imgs[i].style.cssText = "width:" + imgWidth + "px;margin:" + this.gap + "px;";
+Gallery.prototype.renderStyle = function(imgWidth, childColumns) {
 
+	var j = this.domImgs.length;
+	for (var i = 0; i < j; i++) {
+		this.domImgs[i].style.width = imgWidth + "px;";
+		this.addImage(this.domImgs[i],childColumns);
 	}
 };
 /**
- * 负责将图片渲染到页面
+ * 总体负责将图片渲染到页面
  * @return {[type]} [description]
  */
 Gallery.prototype.renderImgs = function() {
-	// this.wrap.innerHTML = this.imgsHtml.join('');
-	// var imgs = document.querySelectorAll('img');
-	var colWidth = Math.floor((wrapWidth - (this.col + 1) * this.gap) / this.col);
-	var childs = this.addChild(this.wrap, colWidth);
-	this.renderStyle(imgs, colWidth, childs);
+	var colWidth = Math.floor((this.wrapWidth - (this.columnNumber+ 1) * this.gap) / this.columnNumber);
+	var childColumns = this.addChild(this.wrap, colWidth);
+	this.renderStyle(colWidth, childColumns);
+	// this.addImage(this.domImgs,childColumns);
 
 };
 /**
  * 为给定包裹层添加子列，将子列的class设为.col,并返回子列
- * @param {Node} ancestor 被添加子列的包裹层
- * @param {Number} width  子列宽度
- * @return {NodeList} 返回子列
+ * @param {Node} 		ancestor       被添加子列的包裹层
+ * @param {Number} 		columWidth     子列宽度
+ * @return {NodeList}   返回子列
  */
-Gallery.prototype.addChild = function(ancestor, width) {
-	var child = null;
-	for (var i = 0; i < colLength; i++) {
-		child = document.createElement('div');
-		child.setAttribute('class', 'col');
-		child.style.width = width + 'px';
-		ancestor.appendChild(child);
+Gallery.prototype.addChild = function(ancestor, columWidth) {
+	var childColumns = [];
+	for (var i = 0; i < this.columnNumber; i++) {
+		childColumns[i] = document.createElement('div');
+		childColumns[i].setAttribute('class', 'col');
+		childColumns[i].style.width = columWidth + 'px';
+		childColumns[i].style.marginRight = this.gap + 'px';
+		ancestor.appendChild(childColumns[i]);
 	}
-	return child;
+	return childColumns;
 };
+
+
+/**
+ * 负责将每一个图片加到页面中对应的列里
+ * @param {Node} image         被加到页面的img元素
+ * @param {Node} childColumns  父包裹层的子列
+ */
+Gallery.prototype.addImage= function(image,childColumns) {
+	var smalleast = this.getSmalleast();
+	childColumns[smalleast].appendChild(image);
+}
+/**
+ * 判断最小的列数
+ * @return {Number} 返回最小列数的数组下标
+ */
+Gallery.prototype.getSmalleast = function(){
+	var length = this.columnNumber;
+	var smalleast = 0;
+	for(var i = 1; i < length; i++){
+		if(this.childColumnsLength[i] < this.childColumnsLength[i-1]){
+			smalleast = i;
+		}
+	}
+	return smalleast;
+}
--- xyg/task_2_44/init.js
+++ xyg/task_2_44/init.js
@@ -1,13 +1,13 @@
 /**
- * 循环创建图片的html格式，图片为placehold.it提供
+ * 循环创建htmlDOM的img标签，图片为placehold.it提供
  * @param  {Number} num        图片数量
  * @param  {Number} lengthBase 图片长度的基数
  * @param  {Number} widthBase  图片宽度的基数
  * @param  {Array} colors      16进制的颜色格式，给图片上色
  * @return {Array}            图片的html格式的数组
  */
-function getImgsHtml(num, lengthBase, widthBase, colors) {
-	var imgs = [];
+function getDomImgs(num, lengthBase, widthBase, colors) {
+	var domImgs = [];
 	var length = 0;
 	var width = 0;
 	var colorLen = colors.length;
@@ -16,18 +16,18 @@ function getImgsHtml(num, lengthBase, widthBase, colors) {
 		index = Math.floor(Math.random() * colorLen);
 		length = Math.round(Math.random() * lengthBase);
 		width = Math.round(Math.random() * widthBase);
-		imgs[i] = '<img src="http://placehold.it/' + 10 + 'x' +  length +
-		 '/' + colors[index] + '">';
+		domImgs[i] = document.createElement('img');
+		domImgs[i].src = "http://placehold.it/" + length + "/" + colors[index];
 	}
-	return imgs;
+	return domImgs;
 }
 
 
 function init() {
 	var colors = ['6D2E5B', '26453D', '0B1013', 'ECB88A', 'ECB88A', 'ECB88A', 'CA7853', '58B2DC', '58B2DC', 'F7C242'];
-	var imgsHtml = getImgsHtml(20, 400, 800, colors);
+	var domImgs = getDomImgs(20, 400, 800, colors);
 	var wrap = document.querySelector('.gallery-wrap');
-	var gallery = new Gallery(imgsHtml, wrap, 16, 4);
+	var gallery = new Gallery(domImgs, wrap, 16, 4);
 	gallery.renderImgs();
 }
 addLoadEvent(init);
